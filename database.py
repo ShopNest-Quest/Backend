d783773b6fc1887d54dfbad6412cc584b7c419d9
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2024-05-05 00:06:37 +0530

    added authentication

--- database.py
+++ database.py
@@ -26,7 +26,7 @@ def check_user_exists(table_name, username):
         return user is not None
     return False
 
-def register_user(table_name, username, password, email, location=None):
+def register(table_name, username, password, email, location=None):
     connection = create_connection()
     if connection:
         cursor = connection.cursor()
@@ -48,3 +48,30 @@ def register_user(table_name, username, password, email, location=None):
             cursor.close()
             connection.close()
     return False
+
+def authenticate(table_name, username, password):
+    connection = create_connection()
+    if connection:
+        cursor = connection.cursor()
+        query = f"SELECT * FROM {table_name} WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, password))
+        user_info = cursor.fetchone()  # Fetch a single row
+        if user_info:
+            # Convert tuple to dictionary for easier access
+            if table_name != "Admin":
+                user_dict = {
+                        'username': user_info[0],
+                        'password': user_info[1],
+                        'isBlocked': user_info[2],
+                        'email_id': user_info[3]
+                }
+            else:
+                user_dict = {
+                        'username': user_info[0],
+                        'password': user_info[1],
+                        'email_id': user_info[2]
+                }
+            return user_dict
+        else:
+            return None  
+    return None
